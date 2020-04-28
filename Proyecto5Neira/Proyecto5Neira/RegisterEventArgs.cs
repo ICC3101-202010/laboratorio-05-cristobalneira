d966ab4433a92d760cf25cd32afb4dcd191b762e
Cristobal <c@iMac-de-Rene.local>
2020-04-27 20:34:27 -0400

    Primer commit

---
+++ Proyecto5Neira/Proyecto5Neira/RegisterEventArgs.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Proyecto5Neira
+{
+    public class RegisterEventArgs : EventArgs
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string VerificationLink { get; set; }
+    }
+}
---
+++ Proyecto5Neira/Proyecto5Neira/SMSSender.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+namespace Proyecto5Neira
+{
+    public class SMSSender
+    {
+        public void OnPasswordChanged(object source, ChangePasswordEventArgs e)
+        {
+            Thread.Sleep(2000);
+            Console.WriteLine($"\nSMS enviado a {e.Number}: \n {e.Username}, te notificamos que la contrasena de tu cuenta PlusCorp ha sido cambiada. \n");
+            Thread.Sleep(2000);
+        }
+    }
+}
